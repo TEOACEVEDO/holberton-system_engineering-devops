a1b99b441175b950e4c78ed96c43d2b0013ed36c
TEOACEVEDO <teoacevedo.ma@gmail.com>
2022-09-08 15:50:37 -0500

    Requests

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+import requests
+from sys import argv
+import json
+
+
+if __name__ == "__main__":
+
+    todos_url = "https://jsonplaceholder.typicode.com/todos/"
+    users_url = "https://jsonplaceholder.typicode.com/users/"
+    id = argv[1]
+
+    name = requests.get(users_url + id).json().get("username")
+    task = requests.get(todos_url, params={"userId": id}).json()
+    total_task = requests.get(
+        todos_url, params={"userId": id, "completed": "true"}).json()
+
+    with open(id + '.json', 'w') as outfile:
+        json.dump({id: [{"task": tt.get("title"), "completed": tt.get(
+            "completed"), "username": name} for tt in task]}, outfile)
---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+from asyncio import tasks
+import requests
+import json
+
+
+if __name__ == "__main__":
+
+    todos_url = "https://jsonplaceholder.typicode.com/todos"
+    users_url = "https://jsonplaceholder.typicode.com/users/"
+
+    all_users = requests.get(users_url).json()
+    new_json = {}
+    for i in all_users:
+        tasks = requests.get(todos_url, params={"userId": i.get("id")}).json()
+
+        list = []
+        for tt in tasks:
+            array = {}
+            array['task'] = tt.get("title")
+            array['completed'] = tt.get("completed")
+            array['username'] = tt.get("username")
+            list.append(array)
+        new_json[i.get("id")] = list
+
+        with open('todo_all_employees.json', 'w') as outfile:
+            json.dump(new_json, outfile)
