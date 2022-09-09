a58405c2e06559369fcb9da66a810fb5202e43d5
TEOACEVEDO <teoacevedo.ma@gmail.com>
2022-09-09 09:39:37 -0500

    Gather data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -2,18 +2,19 @@
 import requests
 from sys import argv
 
+if __name__ == "__main__":
 
-todos_url = "https://jsonplaceholder.typicode.com/todos/"
-users_url = "https://jsonplaceholder.typicode.com/users/"
-id = argv[1]
+    todos_url = "https://jsonplaceholder.typicode.com/todos/"
+    users_url = "https://jsonplaceholder.typicode.com/users/"
+    id = argv[1]
 
-name = requests.get(users_url + id).json()
-task = requests.get(todos_url, params={"userId": id}).json()
-total_task = requests.get(
-    todos_url, params={"userId": id, "completed": "true"}).json()
+    name = requests.get(users_url + id).json()
+    task = requests.get(todos_url, params={"userId": id}).json()
+    total_task = requests.get(
+        todos_url, params={"userId": id, "completed": "true"}).json()
 
-print("Employee {} is done with task ({}/{}):".format(name.get("name"),
-      len(total_task), len(task)))
+    print("Employee {} is done with task ({}/{}):".format(name.get("name"),
+        len(total_task), len(task)))
 
-for task in total_task:
-    print("\t {}".format(task.get("title")))
+    for task in total_task:
+        print("\t {}".format(task.get("title")))
--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""Dictionary of list of dictionaries"""
 import json
 import requests
 
